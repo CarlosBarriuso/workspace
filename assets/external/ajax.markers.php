ec5f903beeb0f1a817ccf7f61d9424872e9875a1
carlos <carlosbarriuso@yahoo.es>
2019-02-09 13:43:03 +0100

    proyecto enviado al repositorio local

---
+++ assets/external/ajax.markers.php
@@ -0,0 +1,147 @@
+<?php
+
+echo json_encode(
+    $markers = array(
+
+    [
+        'id' => 1,
+        'latitude' => 40.488982,
+        'longitude' => -3.880475,
+        'address' => "02120 Peñas de San Pedro, Albacete",
+    ],
+
+    [
+        'id' => 2,
+        'latitude' => 40.488982,
+        'longitude' => -3.880475,
+        'address' => "02120 Peñas de San Pedro, Albacete",
+    ],
+
+    [
+        'id' => 3,
+        'latitude' => 40.488982,
+        'longitude' => -3.880475,
+        'address' => "02120 Peñas de San Pedro, Albacete",
+    ],
+
+    [
+        'id' => 4,
+        'latitude' => 39.001702,
+        'longitude' => -3.880475,
+        'address' => "02120 Peñas de San Pedro, Albacete",
+    ],
+
+    [
+        'id' => 5,
+        'latitude' => 39.001702,
+        'longitude' => -3.880475,
+        'address' => "02120 Peñas de San Pedro, Albacete",
+    ],
+
+    [
+        'id' => 6,
+        'latitude' => 39.001702,
+        'longitude' => -3.880475,
+        'address' => "02120 Peñas de San Pedro, Albacete",
+    ],
+
+    [
+        'id' => 7,
+        'latitude' => 39.001702,
+        'longitude' => -1.858982,
+        'address' => "02120 Peñas de San Pedro, Albacete",
+    ],
+
+    [
+        'id' => 8,
+        'latitude' => 39.001702,
+        'longitude' => -1.858982,
+        'address' => "02120 Peñas de San Pedro, Albacete",
+    ],
+
+    [
+        'id' => 9,
+        'latitude' => 37.370655,
+        'longitude' => -5.98477,
+        'address' => "02120 Peñas de San Pedro, Albacete",
+    ],
+
+    [
+        'id' => 10,
+        'latitude' => 38.72035,
+        'longitude' => -9.128143,
+    ],
+
+    [
+        'id' => 11,
+        'latitude' => 41.302535,
+        'longitude' => 1.998713,
+    ],
+
+    [
+        'id' => 12,
+        'latitude' => 41.302535,
+        'longitude' => 1.998713,
+    ],
+
+    [
+        'id' => 13,
+        'latitude' => 41.302535,
+        'longitude' => 1.998713,
+    ],
+
+    [
+        'id' => 14,
+        'latitude' => 48.67101262,
+        'longitude' => 5.95321655,
+    ],
+
+    [
+        'id' => 15,
+        'latitude' => 48.57478991,
+        'longitude' => 5.81039429,
+    ],
+
+    [
+        'id' => 16,
+        'latitude' => 48.93873912,
+        'longitude' => 6.0383606,
+    ],
+
+    [
+        'id' => 17,
+        'latitude' => 48.39820894,
+        'longitude' => 6.32675171,
+    ],
+
+    [
+        'id' => 18,
+        'latitude' => 48.880328,
+        'longitude' => 2.307258,
+    ],
+
+    [
+        'id' => 19,
+        'latitude' => 48.880284,
+        'longitude' => 2.306721,
+    ],
+
+    [
+        'id' => 20,
+        'latitude' => 48.860342,
+        'longitude' => 2.304597,
+    ],
+
+    [
+        'id' => 21,
+        'latitude' => 48.852549,
+        'longitude' => 2.329574,
+    ],
+
+    [
+        'id' => 22,
+        'latitude' => 48.857124,
+        'longitude' => 2.316699,
+    ],
+
+));
